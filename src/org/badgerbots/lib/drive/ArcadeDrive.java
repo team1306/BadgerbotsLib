0015eea3f2846d0e4ad4621a005592039ba77364
Finn Voichick <fvoichick@gmail.com>
2014-01-25 15:29:20 -0600

    Changed the drive() method to use James's algorithm.

--- src/org/badgerbots/lib/drive/ArcadeDrive.java
+++ src/org/badgerbots/lib/drive/ArcadeDrive.java
@@ -18,24 +18,39 @@ public abstract class ArcadeDrive extends Drive {
 
     protected abstract boolean precision();
 
+    public void drive1() {
+
+            double rotate = joyX();
+            double move = joyY();
+
+            if (Math.abs(rotate) > Math.abs(move)) {
+                rotate += rotate - move;
+            } else {
+                move += move - rotate;
+            }
+
+            if (precision()) {
+                leftJag().set(-posToSpeed((move + rotate) / 2, precSpeed));
+                rightJag().set(posToSpeed((move - rotate) / 2, precSpeed));
+            } else {
+                leftJag().set(-posToSpeed((move + rotate) / 2, maxSpeed));
+                rightJag().set(posToSpeed((move - rotate) / 2, maxSpeed));
+            }
+        }
+
     public void drive() {
 
         double rotate = joyX();
         double move = joyY();
 
-        if (Math.abs(rotate) > Math.abs(move)) {
-            rotate += rotate - move;
-        } else {
-            move += move - rotate;
-        }
+        double radius = Math.sqrt(rotate * rotate + move * move);
 
-        if (precision()) {
-            leftJag().set(-posToSpeed((move + rotate) / 2, precSpeed));
-            rightJag().set(posToSpeed((move - rotate) / 2, precSpeed));
-        } else {
-            leftJag().set(-posToSpeed((move + rotate) / 2, maxSpeed));
-            rightJag().set(posToSpeed((move - rotate) / 2, maxSpeed));
+        if (radius > 1) {
+            rotate /= radius;
+            move /= radius;
         }
+        leftJag().set(-posToSpeed(move + rotate, maxSpeed));
+        rightJag().set(posToSpeed(move - rotate, maxSpeed));
     }
 
 }
