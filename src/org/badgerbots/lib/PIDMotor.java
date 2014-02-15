6c162cb6279e41416c292def764a89cc926309df
Finn Voichick <fvoichick@gmail.com>
2014-02-15 08:21:16 -0600

    Fixed PID calculation and added getEncoderRate() method

--- src/org/badgerbots/lib/PIDMotor.java
+++ src/org/badgerbots/lib/PIDMotor.java
@@ -7,6 +7,7 @@ package org.badgerbots.lib;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Represents a motor controlled with PID. It contains a Jaguar motor and an
@@ -24,9 +25,20 @@ public class PIDMotor implements SpeedController {
 
     private double lastSpeed;
 
+    /**
+     * Creates a new PIDMotor with the given motor, encoder, and PID constants.
+     *
+     * @param jag The Jaguar motor to control
+     * @param enc The encoder that's associated with the motor
+     * @param p The proportional constant
+     * @param i The integral constant. Currently unsupported.
+     * @param d The derivative constant. Currently unsupported.
+     */
     public PIDMotor(Jaguar jag, Encoder enc, double p, double i, double d) {
         motor = jag;
         encoder = enc;
+        encoder.start();
+        lastTime = Timer.getFPGATimestamp();
         this.p = p;
         this.i = 0.0;
         this.d = 0.0;
@@ -39,18 +51,35 @@ public class PIDMotor implements SpeedController {
         }
     }
 
-    private double getMotorOutput(double speed) {
+    /**
+     * Gets the current encoder rate
+     *
+     * @return the average rate of the encoder since this method was last called
+     */
+    public double getEncoderRate() {
+        double distance = encoder.getDistance();
+        double time = lastTime - Timer.getFPGATimestamp();
+
+        lastTime = Timer.getFPGATimestamp();
+        encoder.reset();
+
+        return distance / time;
+    }
+    private double lastTime;
+
+    /**
+     * Sets the speed of the motor, using PID calculation.
+     *
+     * @param speed
+     */
+    public void set(double speed) {
+
         double expected = speed;
-        double error = expected - encoder.getRate();
+        double error = getEncoderRate() - expected;
         double pValue = error * p;
 
         lastSpeed += pValue;
-        return lastSpeed;
-    }
-
-    public void set(double speed) {
-
-        motor.set(getMotorOutput(speed));
+        motor.set(lastSpeed);
     }
 
     public void setP(double p) {
@@ -62,7 +91,7 @@ public class PIDMotor implements SpeedController {
     }
 
     public void set(double speed, byte syncGroup) {
-        motor.set(getMotorOutput(speed), syncGroup);
+        throw new java.lang.IllegalArgumentException("Do not use set(double, byte). Use set(double) instead.");
     }
 
     public void disable() {
